ec1111190dbc0fc78a71da2cc6313a872d97f4e5
Lyndon-Bualat <64567818+Lyndon-Bualat@users.noreply.github.com>
2021-10-21 12:00:58 -0600

    Add files via upload

---
+++ MediaPlayer/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+//using Microsoft.DirectX.AudioVideoPlayback;
+//using WMPLib;
+
+namespace MediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player = new SoundPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                SoundPlayer player = new SoundPlayer(ofd.FileName);
+                player.Play();
+            }
+
+            // WindowsMediaPlayer.Ctlcontrols.play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
