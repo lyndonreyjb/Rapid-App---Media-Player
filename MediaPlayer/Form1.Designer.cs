b95c38b7fcb6f961333f61a7809abed35f4b536b
Salman Virji <salmanvirji@gmail.com>
2021-11-16 17:33:05 -0700

    Added placeholder icons added play pause show and hide functions Added restart function Added Next function Added Folder Browser Added Fullscreen Toggle and function based on lyndons branch (some issues check comments ) Added Menu Strip Added Playlist ( current playlist is bugged need to figure out how to display horizontally)

--- MediaPlayer/Form1.Designer.cs
+++ MediaPlayer/Form1.Designer.cs
@@ -35,12 +35,22 @@ namespace MediaPlayer
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             this.btnFastforward = new System.Windows.Forms.Button();
             this.btnRewind = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.controlsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.btnFullScreen = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBoxPlayList = new System.Windows.Forms.ListBox();
+            this.btnPause = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -49,11 +59,11 @@ namespace MediaPlayer
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(268, 373);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(343, 422);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 52);
+            this.button1.Size = new System.Drawing.Size(53, 42);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Previous";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -63,11 +73,11 @@ namespace MediaPlayer
             this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(358, 378);
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(439, 422);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 42);
+            this.button2.Size = new System.Drawing.Size(53, 42);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Play";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -77,12 +87,13 @@ namespace MediaPlayer
             this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(441, 374);
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(549, 422);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(99, 51);
+            this.button3.Size = new System.Drawing.Size(45, 42);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Skip";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -90,11 +101,11 @@ namespace MediaPlayer
             this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(546, 374);
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(284, 422);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(99, 51);
+            this.button4.Size = new System.Drawing.Size(53, 42);
             this.button4.TabIndex = 3;
-            this.button4.Text = "Repeat";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -103,31 +114,22 @@ namespace MediaPlayer
             this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(163, 373);
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(603, 422);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(99, 52);
+            this.button5.Size = new System.Drawing.Size(46, 42);
             this.button5.TabIndex = 4;
-            this.button5.Text = "Shuffle";
             this.button5.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(17, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(771, 322);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // axWindowsMediaPlayer1
             // 
             this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Top;
             this.axWindowsMediaPlayer1.Enabled = true;
-            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 29);
             this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
             this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
             this.axWindowsMediaPlayer1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 100);
-            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(800, 372);
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(931, 372);
             this.axWindowsMediaPlayer1.TabIndex = 1;
             // 
             // btnFastforward
@@ -136,11 +138,11 @@ namespace MediaPlayer
             this.btnFastforward.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
             this.btnFastforward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnFastforward.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFastforward.Location = new System.Drawing.Point(279, 422);
+            this.btnFastforward.Image = ((System.Drawing.Image)(resources.GetObject("btnFastforward.Image")));
+            this.btnFastforward.Location = new System.Drawing.Point(490, 422);
             this.btnFastforward.Name = "btnFastforward";
-            this.btnFastforward.Size = new System.Drawing.Size(121, 33);
+            this.btnFastforward.Size = new System.Drawing.Size(53, 42);
             this.btnFastforward.TabIndex = 7;
-            this.btnFastforward.Text = "Fastforward";
             this.btnFastforward.UseVisualStyleBackColor = true;
             this.btnFastforward.Click += new System.EventHandler(this.btnFastforward_Click);
             // 
@@ -150,35 +152,145 @@ namespace MediaPlayer
             this.btnRewind.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
             this.btnRewind.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRewind.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRewind.Location = new System.Drawing.Point(406, 422);
+            this.btnRewind.Image = ((System.Drawing.Image)(resources.GetObject("btnRewind.Image")));
+            this.btnRewind.Location = new System.Drawing.Point(393, 422);
             this.btnRewind.Name = "btnRewind";
-            this.btnRewind.Size = new System.Drawing.Size(84, 33);
+            this.btnRewind.Size = new System.Drawing.Size(53, 42);
             this.btnRewind.TabIndex = 8;
-            this.btnRewind.Text = "Rewind";
             this.btnRewind.UseVisualStyleBackColor = true;
             this.btnRewind.Click += new System.EventHandler(this.btnRewind_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.controlsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(931, 29);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.openFolderToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(107, 25);
+            this.toolStripMenuItem1.Text = "Open Media";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+            this.openFolderToolStripMenuItem.Text = "Open Folder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // controlsToolStripMenuItem
+            // 
+            this.controlsToolStripMenuItem.Name = "controlsToolStripMenuItem";
+            this.controlsToolStripMenuItem.Size = new System.Drawing.Size(81, 25);
+            this.controlsToolStripMenuItem.Text = "Controls";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 25);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.FlatAppearance.BorderSize = 0;
+            this.btnFolder.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFolder.Image = ((System.Drawing.Image)(resources.GetObject("btnFolder.Image")));
+            this.btnFolder.Location = new System.Drawing.Point(673, 422);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(42, 42);
+            this.btnFolder.TabIndex = 10;
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // btnFullScreen
+            // 
+            this.btnFullScreen.FlatAppearance.BorderSize = 0;
+            this.btnFullScreen.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnFullScreen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFullScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFullScreen.Image = ((System.Drawing.Image)(resources.GetObject("btnFullScreen.Image")));
+            this.btnFullScreen.Location = new System.Drawing.Point(885, 407);
+            this.btnFullScreen.Name = "btnFullScreen";
+            this.btnFullScreen.Size = new System.Drawing.Size(46, 42);
+            this.btnFullScreen.TabIndex = 11;
+            this.btnFullScreen.UseVisualStyleBackColor = true;
+            this.btnFullScreen.Click += new System.EventHandler(this.btnFullScreen_Click);
+            // 
+            // listBoxPlayList
+            // 
+            this.listBoxPlayList.AllowDrop = true;
+            this.listBoxPlayList.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listBoxPlayList.FormatString = "D";
+            this.listBoxPlayList.FormattingEnabled = true;
+            this.listBoxPlayList.HorizontalScrollbar = true;
+            this.listBoxPlayList.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listBoxPlayList.Location = new System.Drawing.Point(0, 464);
+            this.listBoxPlayList.MultiColumn = true;
+            this.listBoxPlayList.Name = "listBoxPlayList";
+            this.listBoxPlayList.Size = new System.Drawing.Size(931, 160);
+            this.listBoxPlayList.TabIndex = 12;
+            // 
+            // btnPause
+            // 
+            this.btnPause.FlatAppearance.BorderSize = 0;
+            this.btnPause.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPause.Image = ((System.Drawing.Image)(resources.GetObject("btnPause.Image")));
+            this.btnPause.Location = new System.Drawing.Point(439, 422);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(53, 42);
+            this.btnPause.TabIndex = 13;
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(931, 617);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.listBoxPlayList);
+            this.Controls.Add(this.btnFullScreen);
+            this.Controls.Add(this.btnFolder);
             this.Controls.Add(this.btnRewind);
             this.Controls.Add(this.btnFastforward);
             this.Controls.Add(this.axWindowsMediaPlayer1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -189,10 +301,20 @@ namespace MediaPlayer
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.PictureBox pictureBox1;
         public AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
         private System.Windows.Forms.Button btnFastforward;
         private System.Windows.Forms.Button btnRewind;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem controlsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.Button btnFullScreen;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBoxPlayList;
+        private System.Windows.Forms.Button btnPause;
     }
 }
 
--- MediaPlayer/Form1.cs
+++ MediaPlayer/Form1.cs
@@ -18,35 +18,31 @@ namespace MediaPlayer
     {
         SoundPlayer player = new SoundPlayer();
         string vPath;
+        string filename;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) // this is the start button 
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                SoundPlayer player = new SoundPlayer(ofd.FileName);
-                player.Play();
-            }
-
-            // WindowsMediaPlayer.Ctlcontrols.play();
+            axWindowsMediaPlayer1.Ctlcontrols.currentPosition = 0;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e) // this is the play button
         {
-            OpenFileDialog ofd2 = new OpenFileDialog();
-            ofd2.Title = "Open Video";
-            ofd2.Filter = "Video | *.mp4"; // editied to to play video files previously couldnt show video file types
-            ofd2.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            if (ofd2.ShowDialog() == DialogResult.OK)
-            {
-                vPath = ofd2.FileName;
-                axWindowsMediaPlayer1.URL = vPath;
-            }
+
+
+
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+
+            button2.Hide();
+            btnPause.Show();
+
+
+
+
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -72,6 +68,79 @@ namespace MediaPlayer
         private void btnRewind_Click(object sender, EventArgs e)
         {
             axWindowsMediaPlayer1.Ctlcontrols.currentPosition -= 10;
+        }
+
+        private void btnFullScreen_Click(object sender, EventArgs e) // Based on lyndons fullscreen method
+        { 
+            if(WindowState ==  FormWindowState.Normal )  // doesnt resize video player and buttons properly 
+            {
+                this.WindowState = FormWindowState.Maximized; 
+            }
+            else if (WindowState == FormWindowState.Maximized )
+            {
+                this.WindowState = FormWindowState.Normal;
+            } 
+
+           // axWindowsMediaPlayer1.fullScreen = true; thiis makes the video player fullscreen but no controls or exit fullscreen
+        }
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            // FolderBrowserDialog folder = new FolderBrowserDialog();
+
+            // folder.ShowNewFolderButton = true;
+
+            // DialogResult result = folder.ShowDialog();
+            //// d.Filter = "Video | *.mp4";
+            // if (result == DialogResult.OK)
+            // {
+            //    //textBox1.Text = folderDlg.SelectedPath;
+            //     Environment.SpecialFolder root = folder.RootFolder;
+            //     vPath = folder.SelectedPath;
+            //     axWindowsMediaPlayer1.URL = vPath;
+            // }
+
+
+
+            OpenFileDialog ofd2 = new OpenFileDialog();
+            ofd2.Title = "Open Video";
+            ofd2.Filter = "Video | *.mp4"; // editied to to play video files previously couldnt show video file types
+           // ofd2.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            ofd2.Multiselect = true;
+            
+            if (ofd2.ShowDialog() == DialogResult.OK)
+            {
+                filename = ofd2.SafeFileName;
+                vPath = ofd2.FileName;
+                listBoxPlayList.Items.Clear();
+
+                for (int i = 0; i < filename.Length; i++)
+                {
+
+
+
+                    listBoxPlayList.Items.Add(filename[i]) ;
+                   
+                }
+                listBoxPlayList.SelectedIndex = 0;
+               
+                axWindowsMediaPlayer1.URL = vPath;
+              
+            }
+
+
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+            button2.Show();
+            btnPause.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e) // this is the next button
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.next();
         }
     }
 }
